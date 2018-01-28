9c8ffa7c3d459e984d8d25f05614a4e9f8836ddc
zfy <378280137@qq.com>
2018-01-28 20:11:08 +0800

    ..

--- src/main/java/dao/UserDao.java
+++
@@ -1,7 +0,0 @@
-package dao;
-
-import model.User;
-
-public interface UserDao {
-    public User login(String username,String userpassword);
-}
--- src/main/java/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package service;
-
-import model.User;
-
-public interface UserService {
-    public User login(String username,String userpassword);
-}
